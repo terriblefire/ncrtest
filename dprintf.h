5f1c257f72b770145154888987ffd5b59ae20d7d
Stephen Leary <stephen@terriblefire.com>
2025-11-03 22:20:56 +0000

    adding serial port out debugging

---
+++ dprintf.h
@@ -0,0 +1,11 @@
+/*
+ * Dual printf - outputs to both standard console and debug console
+ */
+
+#ifndef DPRINTF_H
+#define DPRINTF_H
+
+/* Dual output printf - outputs to both console and debug */
+void dbgprintf(const char *format, ...);
+
+#endif /* DPRINTF_H */
